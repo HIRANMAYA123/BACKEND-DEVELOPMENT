e2f51a3f62fc1afe55361e351fb9e4dd5ef800b6
Hiranmaya Padhi <padhihiranmaya@gmail.com>
2026-09-07 18:55:08 +0530

    Add evenChecker.js to check even numbers

---
+++ evenChecker.js
@@ -0,0 +1,12 @@
+// evenChecker.js
+
+const isEven = require("./isEven");
+
+console.log("=== Even Number Checker ===");
+
+console.log("10:", isEven(10));
+console.log("7:", isEven(7));
+console.log("24:", isEven(24));
+console.log("15:", isEven(15));
+
+console.log("=== Finished ===");
